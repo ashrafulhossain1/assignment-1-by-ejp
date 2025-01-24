3225a05b0968111fd54402606c2adc9d2e4fd61b
ashrafulhossain1 <ahmeadashraful@gmail.com>
2025-01-24 19:09:38 +0600

    ready for work

---
+++ src/app/profile/page.js
@@ -0,0 +1,34 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function ProfilePage() {
+  const { isAuthenticated, getUser } = getKindeServerSession();
+ 
+  const user = await getUser();
+
+  
+  if (!(await isAuthenticated())) {
+    redirect(`/api/auth/login?post_login_redirect_url=/profile`);
+  }
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-light-gray px-4 py-8">
+      <div className="w-full max-w-md p-6 bg-white rounded-xl shadow-lg space-y-6">
+        <h1 className="text-3xl font-semibold text-center text-primary">
+          Welcome to your profile!
+        </h1>
+        <p className="text-center text-lg text-dark-gray">
+          Your username is:{" "}
+          <span className="font-semibold text-primary">
+            {user?.given_name + " " + user?.family_name || "No username available"}
+          </span>
+        </p>
+        <div className="text-center">
+          <button className="px-6 py-2 bg-secondary text-white rounded-full hover:bg-secondary-dark transition-colors">
+            Edit Profile
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,97 @@
+'use client'
+ 
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+const Navbar = ({user}) => {
+  const pathname = usePathname();
+
+  const links = (
+    <>
+      <li>
+        <Link
+          href="/"
+          className={`${
+            pathname === "/" ? "font-bold border-gray-800 border-b-2" : ""
+          }`}
+        >
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link
+          href="/profile"
+          className={`${
+            pathname === "/profile" ? "font-bold border-gray-800 border-b-2" : ""
+          }`}
+        >
+          Profile
+        </Link>
+      </li>
+    </>
+  );
+  
+
+  return (
+    <nav className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            {links}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">Article Atlas</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{links}</ul>
+      </div>
+      <div className="navbar-end gap-3">
+
+      
+      {
+        user ? <Link href="/api/auth/logout" className="btn btn-sm bg-slate-600 text-white">Log out</Link> 
+        :
+        <div className="">
+          <Link
+          href="/api/auth/login"
+          className="btn btn-sm bg-slate-600 text-white"
+        >
+          Sign in
+        </Link>
+        <Link
+          href="/api/auth/register"
+          className="btn btn-sm bg-slate-600 text-white"
+        >
+          Sign up
+        </Link>
+        </div>
+
+      }
+
+
+
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
